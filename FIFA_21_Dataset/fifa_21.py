bdc3c9a4919f0dd9c41742dbd636866ff6d12999
Datslimtee <fehintolusamuel@gmail.com>
2025-03-04 18:47:45 +0000

    Removed unncessary newline chars,checked players with >10yrs at a club, converted Value, Wages, and Release Clause to numbers, stripped star chars, and plotted scarttered plot of wage vs value

--- FIFA_21_Dataset/fifa_21.py
+++ FIFA_21_Dataset/fifa_21.py
@@ -35,15 +35,29 @@ def pounds_to_kg(weight):
 
 # Apply the conversion function to the 'Weight' column
 df['Weight'] = df['Weight'].apply(pounds_to_kg)
-print (df.dtypes)
+
+# To remove unnecessary newline characters from string columns
+df[df.select_dtypes(['object']).columns] = df.select_dtypes(['object']).apply(lambda x: x.str.replace('\n', ''))
+
+# To Check players with more than 10years at a club
+df['Joined'] = pd.to_datetime(df['Joined'])
+df['Years_at_club']=(pd.Timestamp('now')-df['Joined']).dt.days/365
+players_with_more_than_10_years=df[df['Years_at_club']>10]
 
 
+#To convert 'value', 'wage' and 'Release clause' to numbers
+df['Value'] = df['Value'].str.replace('€', '').str.replace('K', '000').str.replace('M', '000000').astype(float)
+df['Wage'] = df['Wage'].str.replace('€', '').str.replace('K', '000').str.replace('M', '000000').astype(float)
+df['Release Clause'] = df['Release Clause'].str.replace('€', '').str.replace('K', '000').str.replace('M', '000000').astype(float)
 
+#To strip star characters
+df['W/F'] = df['W/F'].str.replace('★', '').astype(float)
+df['SM'] = df['SM'].str.replace('★', '').astype(float)
+df['IR'] = df['IR'].str.replace('★', '').astype(float)
 
-
-
-
-
-
-
-
+#To scatter plot between wage and value
+plt.scatter(df['Wage'],df['Value'])
+plt.xlabel('Wage')
+plt.ylabel('Value')
+plt.title('Wage vs Value')
+plt.show()
